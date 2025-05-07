0d86185890b35a814f21f0a4549424c5828d97fa
Aadarsh-00 <aadarsh.karn@westcliff.edu>
2025-05-07 23:13:50 +0545

    updated_id

---
+++ js-class/script.js
@@ -0,0 +1,21 @@
+let x = 25;
+let y = 10;
+
+// console.log('Product:',x*y);
+// console.log('Sum:',x+y);
+// console.log('Difference:',x-y);
+
+var year = 2080;
+if(year==2082){
+    console.log('Tapai 2082 ma hunuhunchha');
+}
+else if (year>2082){
+    console.log('Yo aauney year ma ke cha tapai ko plan?');
+}
+else if(year<2082){
+  console.log(`${year} Yo bitisake ko samay ho`);
+} 
+// if(x>y){
+//     console.log(x +' is greater than '+ y);
+// }else if(x<y){
+//     console.log(x + ' is less than ' + y); }
